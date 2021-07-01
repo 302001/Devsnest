f6383ccfeaf73b11d7d4647a5d396887bc39bac2
vernika_30 <85216377+302001@users.noreply.github.com>
2021-07-02 00:46:04 +0600

    Update q-1.js

--- THA 7/q-1.js
+++ THA 7/q-1.js
@@ -1,1 +1,14 @@
 
+var student = {
+  name: 'Vernika Agarwal',
+  sclass: 'VI',
+  rollno: 11,
+}
+var ans = Object.keys(student)
+console.log(ans.join(','))
+
+console.log(student)
+
+delete student.rollno
+
+console.log(student)
