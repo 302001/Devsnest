0d14002eaa59dc63d77fce9411f5c2a51a69bad8
vernika_30 <85216377+302001@users.noreply.github.com>
2021-07-04 14:45:09 +0600

    Delete THA_6 directory

--- THA_6/q-2.js
+++
@@ -1,6 +0,0 @@
-function clone(p)
-{
-    return (p);
-}
-document.write('['+clone([1,2,4,0])+']');
-document.write('['+clone([1,2,[4,0]])+']');
--- THA_6/q-4.js
+++
@@ -1,6 +0,0 @@
-function join_Elements(array) {
-    return array.join('+');
-  }
-  const myColors = ['Red', 'Green', 'White', 'Black']
- document.write(join_Elements(myColors))
-  
